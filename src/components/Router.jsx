6b4a7ab46ad741868256542448af2caf99debed1
sidney <ojsidney044@gmail.com>
2024-12-09 09:16:22 +0300

    new router file

---
+++ src/components/Router.jsx
@@ -0,0 +1,8 @@
+function Router(){
+    return(
+        <div>
+        
+        </div>
+    )
+}
+export default Router
--- src/pages/Layout.jsx
+++ src/pages/Layout.jsx
@@ -1,9 +1,15 @@
-import 
+import Nav from "../components/Nav"
+import Footer from "../components/Footer"
 
-function Layout(){
+function Layout({children}){
     return(
-<>
-</>
+<div className="flex flex-col min-h-screen">
+<Nav/>
+<main className="flex-1">
+{children}
+</main>
+<Footer/>
+</div>
     )
 }
 export default Layout
