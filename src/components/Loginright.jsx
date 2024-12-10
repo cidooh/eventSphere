44bafbe7a4389a948fd5db7ba28d76a68d5d84dd
sidney <ojsidney044@gmail.com>
2024-12-11 00:09:30 +0300

    validation on the sign uppage

--- src/components/Loginright.jsx
+++ src/components/Loginright.jsx
@@ -5,6 +5,19 @@ import React, {useState } from "react"
 function Loginright(){
 
     const [error,setError]=useState({})
+    const [formData,setFormData]=useState({})
+
+    function handleChange(e){
+        setFormData((prev)=>({...prev,[e.target.name]:e.target.value}))
+    }
+
+    function handleLogin(e){
+const errors = {}
+!formData.email || formData.email === "" ?(errors.email =true):(errors.email=false)
+!formData.password || formData.password === "" ?(errors.password=true) :(errors.password=false)
+setError(errors)
+console.log(errors)
+    }
 
     return(
         <div className="bg-white mt-24 rounded-lg min-h-screen w-[70pc]">
@@ -12,6 +25,7 @@ function Loginright(){
             <div className="text-6xl font-bold ml-[40%] p-10">
                 <h1>Login</h1>
             </div>
+                {error.email || error.password ? (<p className="text-red-500 text-center">Please fill all the required fields</p>) : <p> </p>}
 <div className="p-10">
    <button className="p-5 border-2 flex ml-[40%] rounded-2xl text-xl hover:bg-[#FFE047]"><FcGoogle className=" w-[30px] h-[30px] "/>Login With Google</button>
    <h1 className="p-6 text-center">OR</h1>  
@@ -19,13 +33,13 @@ function Loginright(){
 
 <div className="ml-[20%]">
     <p>Email Address</p>
-    <input name="email" type="email" placeholder="Enter your e-mail" className="p-4 border-2 w-[500px]"/>
+    <input name="email" type="email" placeholder="Enter your e-mail" className={`p-4 border-2 w-[500px] ${error.email ? "border-red-500" : "border-gray-400" }`} onChange={(e)=>handleChange(e)} value={formData.email && formData.email}/>
 
-<p>Password</p>
-<input name="password" type="password"placeholder="Enter Password" className="border-2 p-4 w-[500px] mt-10"/>
+<p className="mt-10">Password</p>
+<input name="password" type="password"placeholder="Enter Password" className={`border-2 p-4 w-[500px] ${error.password ? "border-red-500" : "border-grey-400"}`} onChange={(e)=>handleChange(e)}/>
 </div>
 
-<button className="p-4 mt-10 border-2 w-[200px] rounded-2xl text-xl bg-[#2B293D] text-white ml-[20%] ">Login</button>
+<button className="p-4 mt-10 border-2 w-[200px] rounded-2xl text-xl bg-[#2B293D] text-white ml-[20%] " onClick={handleLogin}>Login</button>
 <p className=" ml-[20%] p-4">Dont have an account?Sign up</p>
 </div>
         </div>
--- src/components/Signupright.jsx
+++ src/components/Signupright.jsx
@@ -1,13 +1,32 @@
-import { FcGoogle } from "react-icons/fc";
+import React,{useState} from "react"
 
 
 function Signupright(){
+const [error,setError]=useState({})
+const [formData,setFormData]=useState({})
+
+
+function handleChange(e){
+    setFormData((prev) =>({...prev, [e.target.name]:e.target.value}))
+}
+
+function handleSignUp(){
+    const errors = {}
+    !formData.fullName || formData.fullName === "" ? (errors.fullName = true): (errors.fullName = false)
+    !formData.email || formData.email === "" ? (errors.email = true) : (errors.email = false)
+    !formData.password || formData.password === "" ? (errors.password = true) : (errors.password = false)
+    !formData.confirmPassword || formData.confirmPassword === "" ? (errors.confirmPassword = true) : (errors.confirmPassword = false)
+
+    setError(errors)
+}
     return(
         <div className="bg-white mt-24 rounded-lg min-h-screen w-[70pc]">
             <div className="ml-[20%]">
             <div className="text-6xl font-bold ml-[35%] p-10">
                 <h1>Create Account</h1>
             </div>
+            {error.fullName || error.email || error.password || error.confirmPassword ? (<p className="text-red-500 text-center">Please fill all the fields</p>) : (
+          <p> </p>)}
 <div className="p-10">
    
    
@@ -15,22 +34,22 @@ function Signupright(){
 
 <div className="ml-[20%] text-xl p-5">
     <p className="p-4">Full Name</p>
-    <input type="text" placeholder="Enter your full name" className="p-4 border-2 w-[500px]"/>
+    <input name="fullName" type="text" placeholder="Enter your full name" className={`p-4 border-2 w-[500px] ${error.fullName ? "border-red-500" : "border-gray-400"}`}/>
 
 <p  className="p-4">E-mail Address</p>
-<input type="email" placeholder="Enter your email" className="p-4 border-2 w-[500px]"/>
+<input name="email" type="email" placeholder="Enter your email" className={`p-4 border-2 w-[500px] ${error.email ? "border-red-500" : "border-gray-400"}`}/>
 
 <p className="p-4">Password</p>
-<input type="password"placeholder="Enter Password" className="border-2 p-4 w-[500px]"/>
+<input name="password" type="password" placeholder="Enter Password" className={`border-2 p-4 w-[500px] ${error.password ? "border-red-500" : "border-gray-400"}`}/>
 
 <p className="p-4">Confirm Password</p>
-<input type="password"placeholder="Enter Password" className="border-2 p-4 w-[500px]"/>
+<input name="confirmPassword" type="password" placeholder="Re-enter Password" className={`border-2 p-4 w-[500px] ${error.confirmPassword ? "border-red-500" : "border-gray-400"}`}/>
 </div>
 
 
 
 
-<button className="p-4 border-2 w-[200px] rounded-2xl text-xl bg-[#2B293D] text-white ml-[20%] mt-10">Create Account</button>
+<button className="p-4 border-2 w-[200px] rounded-2xl text-xl bg-[#2B293D] text-white ml-[20%] mt-10" onClick={handleSignUp}>Create Account</button>
 <p className="ml-[20%] p-7">Already have an account?Sign in</p>
 </div>
         </div>
